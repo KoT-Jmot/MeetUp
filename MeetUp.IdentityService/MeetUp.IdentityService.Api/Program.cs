bd9d6f861f179235931a7794cea1f5630fbab5cb
Q117 <73116785+KoT-Jmot@users.noreply.github.com>
2023-05-25 21:51:31 +0300

    add configuration

--- MeetUp.IdentityService/MeetUp.IdentityService.Api/Program.cs
+++ MeetUp.IdentityService/MeetUp.IdentityService.Api/Program.cs
@@ -1,15 +1,22 @@
+using MeetUp.IdentityService.Api.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var services = builder.Services;
 
-services.AddControllers();
+var configuration = new ConfigurationBuilder()
+                   .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                   .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: false, reloadOnChange: true)
+                   .Build();
+
+services.ConfigureSqlServer(configuration)
+        .AddControllers();
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 app.UseRouting();
-app.MapControllers();
 
-app.MapGet("/", () => "Hello World!");
+app.MapControllers();
 
 app.Run();
