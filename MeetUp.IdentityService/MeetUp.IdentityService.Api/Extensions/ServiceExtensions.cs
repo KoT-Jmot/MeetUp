10594e0f806f15d5a11308f904eeb19269145ac4
Q117 <73116785+KoT-Jmot@users.noreply.github.com>
2023-05-25 21:50:45 +0300

    add service extensions for sqlServer

--- MeetUp.IdentityService/MeetUp.IdentityService.Api/Extensions/ServiceExtensions.cs
+++ MeetUp.IdentityService/MeetUp.IdentityService.Api/Extensions/ServiceExtensions.cs
@@ -1,6 +1,18 @@
+using MeetUp.IdentityService.Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
 namespace MeetUp.IdentityService.Api.Extensions;
 
-public class ServiceExtensions
+public static class ServiceExtensions
 {
-    
+    public static IServiceCollection ConfigureSqlServer(
+            this IServiceCollection services,
+            IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationContext>(optionsBuilder =>
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+        return services;
+    }
 }
