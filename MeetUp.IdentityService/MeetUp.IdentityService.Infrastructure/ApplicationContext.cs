3acf1d7b89f1c23a43ff01a2bc51a1dbaaa683f1
Q117 <73116785+KoT-Jmot@users.noreply.github.com>
2023-05-25 22:49:29 +0300

    Change folder

--- MeetUp.IdentityService/MeetUp.IdentityService.Infrastructure/ApplicationContext.cs
+++ MeetUp.IdentityService/MeetUp.IdentityService.Infrastructure/ApplicationContext.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
-namespace MeetUp.IdentityService.Infrastructure.Contexts;
+namespace MeetUp.IdentityService.Infrastructure;
 
 public class ApplicationContext : IdentityDbContext<IdentityUser>
 {
