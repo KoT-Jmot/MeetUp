d8c78f458457c25af8bd8b3799fe63d00ebffe9f
Q117 <73116785+KoT-Jmot@users.noreply.github.com>
2023-05-26 10:18:10 +0300

    init db identity data

---
+++ MeetUp.IdentityService/MeetUp.IdentityService.Application/DbInIt/DbInitialize.cs
@@ -0,0 +1,35 @@
+﻿using MeetUp.IdentityService.Application.Utils;
+using Microsoft.AspNetCore.Identity;
+
+namespace MeetUp.IdentityService.Application.DbInIt
+{
+    public static class DbInitialize
+    {
+        public static async Task RolesInitialize(this RoleManager<IdentityRole> roleManager)
+        {
+            var userName = AccountRoles.GetDefaultRole;
+            var userIsInitialize = await roleManager.RoleExistsAsync(userName);
+
+            if (!userIsInitialize)
+                await roleManager.CreateAsync(new IdentityRole(userName));
+
+            var adminName = AccountRoles.GetAdministratorRole;
+            var adminIsInitialize = await roleManager.RoleExistsAsync(adminName);
+
+            if (!adminIsInitialize)
+                await roleManager.CreateAsync(new IdentityRole(adminName));
+
+            var superAdminName = AccountRoles.GetSuperAdministratorRole;
+            var superAdminIsInitialize = await roleManager.RoleExistsAsync(superAdminName);
+
+            if (!superAdminIsInitialize)
+                await roleManager.CreateAsync(new IdentityRole(superAdminName));
+
+            var blockedRole = AccountRoles.GetBlockedRole;
+            var blockedIsInitialize = await roleManager.RoleExistsAsync(blockedRole);
+
+            if (!blockedIsInitialize)
+                await roleManager.CreateAsync(new IdentityRole(blockedRole));
+        }
+    }
+}
