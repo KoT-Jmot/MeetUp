f81e28a4394ab53f91db2da6a3434b77fe3c6cd9
Q117 <73116785+KoT-Jmot@users.noreply.github.com>
2023-05-31 18:45:14 +0300

    add new excaption

---
+++ MeetUp.ЕventsService/MeetUp.EventsService.Application/Utils/Excaption/RequestAccessException.cs
@@ -0,0 +1,9 @@
+﻿namespace MeetUp.EventsService.Application.Utils.Excaption
+{
+    public class RequestAccessException : Exception
+    {
+        public RequestAccessException(string message = "Access was denied!") : base(message)
+        {
+        }
+    }
+}
