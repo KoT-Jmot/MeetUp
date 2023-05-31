4e477aa1ec00184a1589f2eb0151c5efafe75439
Q117 <73116785+KoT-Jmot@users.noreply.github.com>
2023-05-31 17:16:07 +0300

    init controller for categories

---
+++ MeetUp.ЕventsService/MeetUp.EventsService.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MeetUp.EventsService.Api.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public CategoriesController()
+        {
+        }
+    }
+}
