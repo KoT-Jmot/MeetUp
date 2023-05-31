04eabe30b4428152d469b8ef9ef7e4e6a779b873
Q117 <73116785+KoT-Jmot@users.noreply.github.com>
2023-05-31 17:33:30 +0300

    Add migration

--- MeetUp.ЕventsService/MeetUp.EventsService.Infrastructure/Migrations/20230531143238_Eventupdated.Designer.cs
+++ MeetUp.ЕventsService/MeetUp.EventsService.Infrastructure/Migrations/20230531143238_Eventupdated.Designer.cs
@@ -4,6 +4,7 @@ using MeetUp.EventsService.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MeetUp.EventsService.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20230531143238_Eventupdated")]
+    partial class Eventupdated
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MeetUp.ЕventsService/MeetUp.EventsService.Infrastructure/Migrations/20230531143238_Eventupdated.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MeetUp.EventsService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Eventupdated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateStart",
+                table: "Events",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Place",
+                table: "Events",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateStart",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "Place",
+                table: "Events");
+        }
+    }
+}
