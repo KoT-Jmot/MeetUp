0933569b03e813cef48fed0117c4e5a7e1fa6b83
Q117 <73116785+KoT-Jmot@users.noreply.github.com>
2023-05-31 16:44:17 +0300

    init controllers folder

---
+++ MeetUp.ЕventsService/MeetUp.EventsService.Api/Controllers/EventsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MeetUp.EventsService.Api.Controllers
+{
+    public class EventsController : Controller
+    {
+
+        public EventsController() { }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateEventAsync()
+        {
+
+            return Created(nameof(CreateEventAsync),123);
+        }
+    }
+}
