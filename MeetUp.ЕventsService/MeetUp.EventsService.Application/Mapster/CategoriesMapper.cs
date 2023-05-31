6fddc1860b02729b0ffd097f5646fe459ca64b0a
Q117 <73116785+KoT-Jmot@users.noreply.github.com>
2023-05-31 18:45:34 +0300

    add mapster

---
+++ MeetUp.ЕventsService/MeetUp.EventsService.Application/Mapster/CategoriesMapper.cs
@@ -0,0 +1,16 @@
+﻿using MeetUp.EventsService.Application.DTOs.InputDto.CategoryDto;
+using MeetUp.EventsService.Application.DTOs.OutputDto;
+using MeetUp.EventsService.Infrastructure.Models;
+using Mapster;
+
+namespace MeetUp.EventsService.Application.Mapster
+{
+    public class CategoriesMapper : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<CategoryDto, Category>();
+            config.NewConfig<Category, OutputCategoryDto>();
+        }
+    }
+}
---
+++ MeetUp.ЕventsService/MeetUp.EventsService.Application/Mapster/EventsMapper.cs
@@ -0,0 +1,16 @@
+﻿using MeetUp.EventsService.Application.DTOs.InputDto.EventDto;
+using MeetUp.EventsService.Application.DTOs.OutputDto;
+using MeetUp.EventsService.Infrastructure.Models;
+using Mapster;
+
+namespace MeetUp.EventsService.Application.Mapster
+{
+    public class EventsMapper : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<EventDto, Event>();
+            config.NewConfig<Event, OutputEventDto>();
+        }
+    }
+}
