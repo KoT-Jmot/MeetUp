e566a8642cdbd63db420a7b29ffb90033267986a
Q117 <73116785+KoT-Jmot@users.noreply.github.com>
2023-05-31 17:28:53 +0300

    Update Event.cs

--- MeetUp.ЕventsService/MeetUp.EventsService.Infrastructure/Models/Event.cs
+++ MeetUp.ЕventsService/MeetUp.EventsService.Infrastructure/Models/Event.cs
@@ -4,8 +4,10 @@
     {
         public string? Title { get; set; }
         public string? Description { get; set; }
-        public string? SponsorId { get; set; }
+        public string? Place { get; set; }
+        public DateTime? DateStart { get; set; }
         public DateTime CreateDate { get; set; } = DateTime.UtcNow;
+        public string? SponsorId { get; set; }
         public Guid categoryId { get; set; }
         public virtual Category? Category { get; set; }
     }
