e708bd4d90c299d240b174e858cce6da18d2fc3c
Q117 <73116785+KoT-Jmot@users.noreply.github.com>
2023-06-01 14:03:12 +0300

    add new version of migration

---
+++ MeetUp.ЕventsService/MeetUp.EventsService.Infrastructure/Migrations/20230601104959_updateEvent.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MeetUp.EventsService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateEvent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_Categories_categoryId",
+                table: "Events");
+
+            migrationBuilder.RenameColumn(
+                name: "categoryId",
+                table: "Events",
+                newName: "CategoryId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Events_categoryId",
+                table: "Events",
+                newName: "IX_Events_CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_Categories_CategoryId",
+                table: "Events",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_Categories_CategoryId",
+                table: "Events");
+
+            migrationBuilder.RenameColumn(
+                name: "CategoryId",
+                table: "Events",
+                newName: "categoryId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Events_CategoryId",
+                table: "Events",
+                newName: "IX_Events_categoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_Categories_categoryId",
+                table: "Events",
+                column: "categoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
