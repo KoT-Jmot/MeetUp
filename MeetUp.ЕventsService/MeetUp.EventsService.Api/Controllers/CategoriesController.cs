5985abfc0782b4e0fdec810ca4909cebe4d35789
Q117 <73116785+KoT-Jmot@users.noreply.github.com>
2023-06-01 13:35:46 +0300

    create controllers for categories and events

--- MeetUp.ЕventsService/MeetUp.EventsService.Api/Controllers/CategoriesController.cs
+++ MeetUp.ЕventsService/MeetUp.EventsService.Api/Controllers/CategoriesController.cs
@@ -1,11 +1,71 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MeetUp.EventsService.Api.Features;
+using MeetUp.EventsService.Application.Contracts;
+using MeetUp.EventsService.Application.DTOs.InputDto.CategoryDto;
+using MeetUp.EventsService.Application.DTOs.OutputDto;
+using MeetUp.EventsService.Application.RequestFeatures;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MeetUp.EventsService.Api.Controllers
 {
+
+    [Route("Categories")]
     public class CategoriesController : Controller
     {
-        public CategoriesController()
+        private readonly ICategoryService _categoryManager;
+
+        public CategoriesController(ICategoryService categoryManager)
         {
+            _categoryManager = categoryManager;
+        }
+
+        public async Task<IActionResult> GetAllCategoriesAsync(
+            [FromQuery] CategoryQueryDto categoryQuery,
+            CancellationToken cancellationToken)
+        {
+            var categoeies = await _categoryManager.GetAllCategoriesAsync(categoryQuery, cancellationToken);
+
+            return new PagingActionResult<PagedList<OutputCategoryDto>>(categoeies);
+        }
+
+        [HttpGet("{categoryId}")]
+        public async Task<IActionResult> GetCategoryByIdAsync(
+            [FromRoute] Guid categoryId,
+            CancellationToken cancellationToken)
+        {
+            var category = await _categoryManager.GetCategoryByIdAsync(categoryId, cancellationToken);
+
+            return Ok(category);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategoryAsync(
+            [FromBody] CategoryDto categoryDto,
+            CancellationToken cancellationToken)
+        {
+            var categoryId = await _categoryManager.CreateCategoryAsync(categoryDto, cancellationToken);
+
+            return Created(nameof(CreateCategoryAsync), categoryId);
+        }
+
+        [HttpDelete("{categoryId}")]
+        public async Task<IActionResult> DeleteCategoryAsync(
+            [FromRoute] Guid categoryId,
+            CancellationToken cancellationToken)
+        {
+            await _categoryManager.DeleteCategoryByIdAsync(categoryId, cancellationToken);
+
+            return NoContent();
+        }
+
+        [HttpPut("{categoryId}")]
+        public async Task<IActionResult> UpdateCategoryAsync(
+            [FromRoute] Guid categoryId,
+            [FromBody] CategoryDto categoryDto,
+            CancellationToken cancellationToken)
+        {
+            var updatingCategoryId = await _categoryManager.UpdateCategoryByIdAsync(categoryId, categoryDto, cancellationToken);
+
+            return Ok(updatingCategoryId);
         }
     }
 }
--- MeetUp.ЕventsService/MeetUp.EventsService.Api/Controllers/EventsController.cs
+++ MeetUp.ЕventsService/MeetUp.EventsService.Api/Controllers/EventsController.cs
@@ -1,17 +1,77 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MeetUp.EventsService.Api.Features;
+using MeetUp.EventsService.Application.Contracts;
+using MeetUp.EventsService.Application.DTOs.InputDto.EventDto;
+using MeetUp.EventsService.Application.DTOs.OutputDto;
+using MeetUp.EventsService.Application.RequestFeatures;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MeetUp.EventsService.Api.Controllers
 {
+    [Route("Products")]
     public class EventsController : Controller
     {
+        private readonly IEventService _eventManager;
 
-        public EventsController() { }
+        public EventsController(IEventService eventManager)
+        {
+            _eventManager = eventManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllEventsAsync(
+                    [FromQuery] EventQueryDto eventQuery,
+                    CancellationToken cancellationToken)
+        {
+            var exectlEvent = await _eventManager.GetAllEventsAsync(eventQuery, cancellationToken);
+
+            return new PagingActionResult<PagedList<OutputEventDto>>(exectlEvent);
+        }
+
+        [HttpGet("{eventId}")]
+        public async Task<IActionResult> GetEventByIdAsync(
+            [FromRoute] Guid eventId,
+            CancellationToken cancellationToken)
+        {
+            var product = await _eventManager.GetEventByIdAsync(eventId, cancellationToken);
+
+            return Ok(product);
+        }
 
         [HttpPost]
-        public async Task<IActionResult> CreateEventAsync()
+        public async Task<IActionResult> CreateEventAsync(
+            [FromBody] EventDto eventDto,
+            CancellationToken cancellationToken)
         {
+            var userId = User.GetUserId();
 
-            return Created(nameof(CreateEventAsync),123);
+            var eventId = await _eventManager.CreateEventBySponserIdAsync(eventDto, userId, cancellationToken);
+
+            return Created(nameof(CreateEventAsync), eventId);
+        }
+
+        [HttpDelete("{eventId}")]
+        public async Task<IActionResult> DeleteEventByIdAsync(
+            [FromRoute] Guid eventId,
+            CancellationToken cancellationToken)
+        {
+            var sponserId = User.GetUserId();
+
+            await _eventManager.DeleteEventByIdAndSponserIdAsync(sponserId, eventId, cancellationToken);
+
+            return NoContent();
+        }
+
+        [HttpPut("{eventId}")]
+        public async Task<IActionResult> UpdateProductAsync(
+            [FromRoute] Guid eventId,
+            [FromBody] EventDto eventDto,
+            CancellationToken cancellationToken)
+        {
+            var sponserId = User.GetUserId();
+
+            var updatedEventId = await _eventManager.UpdateEventByIdAndSponserIdAsync(eventId, eventDto, sponserId, cancellationToken);
+
+            return Ok(updatedEventId);
         }
     }
 }
