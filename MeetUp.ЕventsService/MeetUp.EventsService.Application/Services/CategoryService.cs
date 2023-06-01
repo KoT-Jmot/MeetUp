ff5a8905b3841ca8127e76630a4a65b4b9057156
Q117 <73116785+KoT-Jmot@users.noreply.github.com>
2023-06-01 10:52:36 +0300

    Update CategoryService.cs

--- MeetUp.ЕventsService/MeetUp.EventsService.Application/Services/CategoryService.cs
+++ MeetUp.ЕventsService/MeetUp.EventsService.Application/Services/CategoryService.cs
@@ -1,8 +1,12 @@
 ﻿using MeetUp.EventsService.Application.DTOs.InputDto.CategoryDto;
 using MeetUp.EventsService.Application.Utils.Excaption;
+using MeetUp.EventsService.Application.RequestFeatures;
+using MeetUp.EventsService.Application.DTOs.OutputDto;
 using MeetUp.EventsService.Infrastructure.Contracts;
 using MeetUp.EventsService.Application.Contracts;
 using MeetUp.EventsService.Infrastructure.Models;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.EntityFrameworkCore;
 using FluentValidation;
 using Mapster;
 
@@ -19,6 +23,42 @@ namespace MeetUp.EventsService.Application.Services
         {
             _repositoryManager = repositoryManager;
             _categoryValidator = categoryValidator;
+        }
+
+        public async Task<OutputCategoryDto> GetCategoryByIdAsync(
+            Guid categoryId,
+            CancellationToken cancellationToken)
+        {
+            var category = await _repositoryManager.Categories.GetByIdAsync(categoryId, trackChanges: false, cancellationToken);
+
+            if (category is null)
+                throw new EntityNotFoundException("Category was not found!");
+
+            var outputCategory = category.Adapt<OutputCategoryDto>();
+
+            return outputCategory;
+        }
+        public async Task<PagedList<OutputCategoryDto>> GetAllCategoriesAsync(
+            CategoryQueryDto categoryQuery,
+            CancellationToken cancellationToken)
+        {
+            var categories = _repositoryManager.Categories.GetAll();
+
+            if (!categoryQuery.Name.IsNullOrEmpty())
+                categories = categories.Where(c => c.Name!.Contains(categoryQuery.Name!));
+
+            categories = categories.OrderBy(c => c.Name);
+            var totalCount = await categories.CountAsync(cancellationToken);
+
+            var pagingCategories = await categories
+                                        .Skip((categoryQuery.PageNumber - 1) * categoryQuery.PageSize)
+                                        .Take(categoryQuery.PageSize)
+                                        .ToListAsync(cancellationToken);
+
+            var outputCategories = pagingCategories.Adapt<IEnumerable<OutputCategoryDto>>();
+            var categoriesWithMetaData = PagedList<OutputCategoryDto>.ToPagedList(outputCategories, categoryQuery.PageNumber, totalCount, categoryQuery.PageSize);
+
+            return categoriesWithMetaData;
         }
 
         public async Task<Guid> CreateCategoryAsync(
