470678720faa76ed5740a640fad10001c7c50d41
Q117 <73116785+KoT-Jmot@users.noreply.github.com>
2023-06-01 13:35:16 +0300

    Create ClaimsConfiguration.cs

---
+++ MeetUp.ЕventsService/MeetUp.EventsService.Application/RequestFeatures/ClaimsConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace MeetUp.EventsService.Application.RequestFeatures
+{
+    public static class ClaimsConfiguration
+    {
+        public static string GetUserId(this ClaimsPrincipal User)
+        {
+            return User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)!.Value;
+        }
+    }
+}
