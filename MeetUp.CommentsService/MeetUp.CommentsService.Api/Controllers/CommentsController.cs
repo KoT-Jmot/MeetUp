9454596ddbc3646b2b7179f922160761a5d8a789
Q117 <73116785+KoT-Jmot@users.noreply.github.com>
2023-06-14 10:44:04 +0300

    Create CommentsController.cs

---
+++ MeetUp.CommentsService/MeetUp.CommentsService.Api/Controllers/CommentsController.cs
@@ -0,0 +1,40 @@
+﻿using MeetUp.CommentsService.Application.RequestFeatures;
+using MeetUp.CommentsService.Application.DTOs.OutputDto;
+using MeetUp.CommentsService.Application.DTOs.InputDto;
+using MeetUp.CommentsService.Application.Contracts;
+using MeetUp.CommentsService.Api.Features;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeetUp.CommentsService.Api.Controllers
+{
+    [Route("comment")]
+    public class CommentsController : Controller
+    {
+        private readonly ICommentService _commentManager;
+
+        public CommentsController(ICommentService commentManager)
+        {
+            _commentManager = commentManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCommentsAsync(
+            [FromQuery] CommentQueryDto commentQuery,
+            CancellationToken cancellationToken)
+        {
+            var comments = await _commentManager.GetAllCommentsAsync(commentQuery, cancellationToken);
+
+            return new PagingActionResult<PagedList<OutputCommentDto>>(comments);
+        }
+
+        [HttpGet("{commentId}")]
+        public async Task<IActionResult> GetCommentByIdAsync(
+            [FromRoute] Guid commentId,
+            CancellationToken cancellationToken)
+        {
+            var comment = await _commentManager.GetCommentByIdAsync(commentId, cancellationToken);
+
+            return Ok(comment);
+        }
+    }
+}
