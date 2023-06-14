16f67d9100e960fa78fc37bcfcf840e7654dbfda
Q117 <73116785+KoT-Jmot@users.noreply.github.com>
2023-06-14 10:15:13 +0300

    Create CommentValidator.cs

---
+++ MeetUp.CommentsService/MeetUp.CommentsService.Application/Validation/CommentValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using MeetUp.CommentsService.Application.DTOs.InputDto;
+
+namespace MeetUp.CommentsService.Application.Validation
+{
+    public class CommentValidator : AbstractValidator<CommentDto>
+    {
+        public CommentValidator()
+        {
+            RuleFor(c => c.Text)
+               .NotEmpty()
+               .NotNull()
+               .MaximumLength(300)
+               .WithMessage("Incorrect comment text!");
+
+            RuleFor(c => c.EventId)
+               .NotEmpty()
+               .NotNull()
+               .WithMessage("Incorrect event data!");
+        }
+    }
+}
