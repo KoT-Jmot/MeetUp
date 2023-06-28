64fb973fb349c31f9ca1f92025c580aa02df499b
Q117 <73116785+KoT-Jmot@users.noreply.github.com>
2023-06-28 15:07:59 +0300

    Create ServicesDataFactory.cs

---
+++ MeetUp.IdentityService/MeetUp.IdentityService.Tests/UnitTests/ServicesTests/ServicesDataFactory.cs
@@ -0,0 +1,38 @@
+﻿using MeetUp.IdentityService.Application.DTOs.InputDto;
+using Microsoft.AspNetCore.Identity;
+
+namespace MeetUp.IdentityService.Tests.UnitTests.ServicesTests
+{
+    public static class ServicesDataFactory
+    {
+        public static UserForRegistrationDto GetUserForRegistrationDto()
+        {
+            return new UserForRegistrationDto
+            {
+                UserName = "ivan",
+                Email = "test@gmail.com",
+                PhoneNumber = "+375112345781",
+                Password = "aaa111aaa"
+            };
+        }
+        public static UserForLoginDto GetUserForLoginDto()
+        {
+            return new UserForLoginDto
+            {
+                Email = "test@gmail.com",
+                Password = "aaa111aaa"
+            };
+        }
+
+        public static IdentityUser GetUserEntity()
+        {
+            return new IdentityUser
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserName = "ivan",
+                Email = "test@gmail.com",
+                PhoneNumber = "+375112345781"
+            };
+        }
+    }
+}
