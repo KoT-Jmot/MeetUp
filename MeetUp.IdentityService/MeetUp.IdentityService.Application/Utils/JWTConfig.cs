7cedfbbe8b1540bcf75971c41c8a44ff2c1f3dc6
Q117 <73116785+KoT-Jmot@users.noreply.github.com>
2023-06-28 15:07:14 +0300

    Update JWTConfig.cs

--- MeetUp.IdentityService/MeetUp.IdentityService.Application/Utils/JWTConfig.cs
+++ MeetUp.IdentityService/MeetUp.IdentityService.Application/Utils/JWTConfig.cs
@@ -6,13 +6,15 @@ namespace MeetUp.IdentityService.Application.Utils
     {
         private readonly IConfiguration _configuration;
 
+        //public JWTConfig() { }
+
         public JWTConfig(IConfiguration configuration)
         {
             _configuration = configuration.GetSection("JwtSettings");
         }
 
-        public string GetValidIssuer() => _configuration.GetSection("validIssuer").Value;
-        public string GetValidAudience() => _configuration.GetSection("validAudience").Value;
-        public double GetExpires() => Convert.ToDouble(_configuration.GetSection("expires").Value);
+        public virtual string GetValidIssuer() => _configuration.GetSection("validIssuer").Value;
+        public virtual string GetValidAudience() => _configuration.GetSection("validAudience").Value;
+        public virtual double GetExpires() => Convert.ToDouble(_configuration.GetSection("expires").Value);
     }
 }
