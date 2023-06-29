1c29d95d6148b5d61ef9793ffdf9e907d956cd16
Q117 <73116785+KoT-Jmot@users.noreply.github.com>
2023-06-29 20:16:22 +0300

    add unit-tests for controllers

---
+++ MeetUp.IdentityService/MeetUp.IdentityService.Tests/UnitTests/ControllersTests/AccountControllerTests.cs
@@ -0,0 +1,84 @@
+﻿using MeetUp.IdentityService.Application.Contracts;
+using MeetUp.IdentityService.Api.Controllers;
+using Moq;
+using MeetUp.IdentityService.Application.DTOs.InputDto;
+using MeetUp.IdentityService.Application.DTOs.QueryDto;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using MeetUp.IdentityService.Api.Actions;
+using MeetUp.IdentityService.Application.DTOs.OutputDto;
+using MeetUp.IdentityService.Application.RequestFeatures;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace MeetUp.IdentityService.Tests.UnitTests.ControllersTests
+{
+    public class AccountControllerTests
+    {
+        private readonly Mock<IAccountService> _accountService;
+        private readonly AccountController _accountController;
+
+        public AccountControllerTests()
+        {
+            _accountService = new Mock<IAccountService>();
+
+            _accountService.Setup(r => r.SignUpAsync(It.IsAny<UserForRegistrationDto>(), It.IsAny<CancellationToken>())).ReturnsAsync(Guid.NewGuid().ToString());
+            _accountService.Setup(r => r.SignInAsync(It.IsAny<UserForLoginDto>(), It.IsAny<CancellationToken>())).ReturnsAsync(Guid.NewGuid().ToString());
+            _accountService.Setup(r => r.GetUserByEmail(It.IsAny<string>())).ReturnsAsync(ControllersDataFactory.GetOutputUserDto());
+            _accountService.Setup(r => r.GetAllUsersAsync(It.IsAny<UserQueryDto>(), It.IsAny<CancellationToken>())).ReturnsAsync(ControllersDataFactory.GetAllOutputUsersDto());
+
+            _accountController = new AccountController(_accountService.Object);
+        }
+
+        [Fact]
+        public async Task SignInAsync_WithCorrectData_ShouldReturnStatusCode200WithData()
+        {
+            //Arrange
+            var userDto = ControllersDataFactory.GetUserForLoginDto();
+
+            //Act
+            var result = await _accountController.SignInAsync(userDto, default);
+
+            //Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task SignUpAsync_WithCorrectData_ShouldReturnStatusCodeCreatedWithData()
+        {
+            //Arrange
+            var userDto = ControllersDataFactory.GetUserForRegistrationDto();
+
+            //Act
+            var result = await _accountController.SignUpAsync(userDto, default);
+
+            //Assert
+            Assert.NotNull(result);
+        }
+        [Fact]
+        public async Task GetUserByEmailAsync_WithCorrectData_ShouldReturnStatusCode200WithData()
+        {
+            //Arrange
+            var userEmail = ControllersDataFactory.GetOutputUserDto().Email;
+
+            //Act
+            var result = await _accountController.GetUserByEmailAsync(userEmail);
+
+            //Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task GetAllUsersAsync_WithCorrectData_ShouldReturnStatusCode200WithData()
+        {
+            //Arrange
+            var userQuery = new UserQueryDto();
+
+            //Act
+            var result = await _accountController.GetAllUsersAsync(userQuery, default);
+
+            //Assert
+            Assert.NotNull(result);
+        }
+    }
+}
---
+++ MeetUp.IdentityService/MeetUp.IdentityService.Tests/UnitTests/ControllersTests/ControllersDataFactory.cs
@@ -0,0 +1,68 @@
+﻿using MeetUp.IdentityService.Application.DTOs.InputDto;
+using MeetUp.IdentityService.Application.DTOs.OutputDto;
+using MeetUp.IdentityService.Application.RequestFeatures;
+using Microsoft.AspNetCore.Identity;
+
+namespace MeetUp.IdentityService.Tests.UnitTests.ControllersTests
+{
+    public static class ControllersDataFactory
+    {
+        public static UserForLoginDto GetUserForLoginDto()
+        {
+            return new UserForLoginDto
+            {
+                Email = "test@gmail.com",
+                Password = "aaa111aaa"
+            };
+        }
+        public static UserForRegistrationDto GetUserForRegistrationDto()
+        {
+            return new UserForRegistrationDto
+            {
+                UserName = "ivan",
+                Email = "test@gmail.com",
+                PhoneNumber = "+375112345781",
+                Password = "aaa111aaa"
+            };
+        }
+        public static OutputUserDto GetOutputUserDto()
+        {
+            return new OutputUserDto
+            {
+                Id = "21f85435-204d-4e1f-80bb-08db734a088b",
+                UserName = "ivan",
+                Email = "test@gmail.com",
+                PhoneNumber = "+375112345781"
+            };
+        }
+        public static PagedList<OutputUserDto> GetAllOutputUsersDto()
+        {
+            var outputUsers = new List<OutputUserDto>()
+            {
+                new OutputUserDto()
+                {
+                    Id = "21f85435-204d-4e1f-80bb-08db734a088b",
+                    UserName = "ivan",
+                    Email = "test@gmail.com",
+                    PhoneNumber = "+375112345781"
+                },
+                new OutputUserDto()
+                {
+                    Id = "52fc493e-ed46-4df7-5544-08db734a420d",
+                    UserName = "vlad",
+                    Email = "vlad@gmail.com",
+                    PhoneNumber = "+375295712360"
+                },
+                new OutputUserDto()
+                {
+                    Id = "c7264143-e47a-42e4-b97a-29d02088282a",
+                    UserName = "kirill",
+                    Email = "kirill@gmail.com",
+                    PhoneNumber = "+375103912871"
+                }
+            };
+
+            return PagedList<OutputUserDto>.ToPagedList(outputUsers, 3, 1, 3);
+        }
+    }
+}
