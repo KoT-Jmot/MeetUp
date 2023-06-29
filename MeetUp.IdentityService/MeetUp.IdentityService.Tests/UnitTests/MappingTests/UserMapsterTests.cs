81817698449ab28a883616e2b19e1e05d5fb9cee
Q117 <73116785+KoT-Jmot@users.noreply.github.com>
2023-06-29 11:05:27 +0300

    unit-tests for mapster

---
+++ MeetUp.IdentityService/MeetUp.IdentityService.Tests/UnitTests/MappingTests/UserMapsterTests.cs
@@ -0,0 +1,24 @@
+﻿using Mapster;
+using MeetUp.IdentityService.Application.Mapster;
+using Microsoft.AspNetCore.Identity;
+
+namespace MeetUp.IdentityService.Tests.UnitTests.MappingTests
+{
+    public class UserMapsterTests
+    {
+        [Fact]
+        public void TestMapping()
+        {
+            // Arrange
+            var config = new TypeAdapterConfig();
+            var register = new IdentityUserMapper();
+            register.Register(config);
+
+            // Act
+            var mappings = config.RuleMap;
+
+            // Assert
+            Assert.NotEmpty(mappings);
+        }
+    }
+}
