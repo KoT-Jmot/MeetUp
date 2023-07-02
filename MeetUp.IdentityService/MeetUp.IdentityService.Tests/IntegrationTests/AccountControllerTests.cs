520901c3fcef420e859e27f1b4b99f492e6375c5
Q117 <73116785+KoT-Jmot@users.noreply.github.com>
2023-07-03 01:23:56 +0300

    update accountcontroller integration tests

--- MeetUp.IdentityService/MeetUp.IdentityService.Tests/IntegrationTests/AccountControllerTests.cs
+++ MeetUp.IdentityService/MeetUp.IdentityService.Tests/IntegrationTests/AccountControllerTests.cs
@@ -1,66 +1,34 @@
-﻿using MeetUp.IdentityService.Application.DTOs.OutputDto;
-using MeetUp.IdentityService.Application.RequestFeatures;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
 
 namespace MeetUp.IdentityService.Tests.IntegrationTests
 {
-    public class AccountControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    public class AccountControllerTests
     {
-        private readonly WebApplicationFactory<Program> _factory;
+        private readonly HttpClient _client;
 
-        public AccountControllerTests(WebApplicationFactory<Program> factory)
+        public AccountControllerTests()
         {
-            factory = factory.WebApplicationFactoryConfig();
+            var webHost = FactoryConfiguration.WebApplicationFactoryConfig();
 
-            Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Development");
+            _client = webHost.CreateClient();
         }
 
         [Theory]
         [InlineData("/account")]
-        public async Task GetAllUsersAsync_SendRequest_ShouldReturnOk(string url)
+        [InlineData("/account/test@gmail.com")]
+        public async Task GetUsersAsync_SendRequest_ShouldReturnOk(string url)
         {
             // Arrange
-            var client = _factory.CreateClient();
 
             // Act
-            var response = await client.GetAsync(url);
+            var response = await _client.GetAsync(url);
 
             // Assert
             response.EnsureSuccessStatusCode();
 
             Assert.Equal("application/json; charset=utf-8", response.Content.Headers.ContentType.ToString());
         }
-
-        public static PagedList<OutputUserDto> GetAllOutputUsersDto()
-        {
-            var outputUsers = new List<OutputUserDto>()
-            {
-                new OutputUserDto()
-                {
-                    Id = "21f85435-204d-4e1f-80bb-08db734a088b",
-                    UserName = "ivan",
-                    Email = "test@gmail.com",
-                    PhoneNumber = "+375112345781"
-                },
-                new OutputUserDto()
-                {
-                    Id = "52fc493e-ed46-4df7-5544-08db734a420d",
-                    UserName = "vlad",
-                    Email = "vlad@gmail.com",
-                    PhoneNumber = "+375295712360"
-                },
-                new OutputUserDto()
-                {
-                    Id = "c7264143-e47a-42e4-b97a-29d02088282a",
-                    UserName = "kirill",
-                    Email = "kirill@gmail.com",
-                    PhoneNumber = "+375103912871"
-                }
-            };
-
-            return PagedList<OutputUserDto>.ToPagedList(outputUsers, 3, 1, 3);
-        }
     }
-
-
 }
