69d23ce028d44a357aacc3fd79f9e955519570b7
Q117 <73116785+KoT-Jmot@users.noreply.github.com>
2023-07-03 01:24:08 +0300

    Update FactoryConfiguration.cs

--- MeetUp.IdentityService/MeetUp.IdentityService.Tests/IntegrationTests/FactoryConfiguration.cs
+++ MeetUp.IdentityService/MeetUp.IdentityService.Tests/IntegrationTests/FactoryConfiguration.cs
@@ -1,20 +1,57 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using MeetUp.IdentityService.Infrastructure;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace MeetUp.IdentityService.Tests.IntegrationTests
 {
     public static class FactoryConfiguration
     {
-        public static WebApplicationFactory<Program> WebApplicationFactoryConfig(this WebApplicationFactory<Program> webHost)
+        public static WebApplicationFactory<Program> WebApplicationFactoryConfig()
         {
-            webHost.WithWebHostBuilder(builder =>
+            var webHost = new WebApplicationFactory<Program>().WithWebHostBuilder(builder =>
             {
-                builder.ConfigureServices(services =>
+                builder.ConfigureTestServices(services =>
                 {
+                    var dbContextDescriptor = services.SingleOrDefault(d =>
+                        d.ServiceType.Equals(typeof(DbContextOptions<ApplicationContext>)));
 
+                    if (dbContextDescriptor != null)
+                    {
+                        services.Remove(dbContextDescriptor);
+                    }
+
+                    services.AddDbContextPool<ApplicationContext>(options =>
+                    {
+                        options.UseInMemoryDatabase(Guid.NewGuid().ToString());
+                    });
                 });
             });
 
+            ConfigEnvironment();
+
+            webHost.ConfigUserManagerFactoryAsync().Wait();
+
             return webHost;
+        }
+
+        private static async Task ConfigUserManagerFactoryAsync(this WebApplicationFactory<Program> webHost)
+        {
+            var userManager = webHost.Services.CreateScope().ServiceProvider.GetService<UserManager<IdentityUser>>();
+
+            foreach (var user in DataFactory.GetAllUsersEntity())
+            {
+                await userManager.CreateAsync(user, DataFactory.UserPassword);
+            }
+        }
+
+        private static void ConfigEnvironment()
+        {
+            Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Development");
+            Environment.SetEnvironmentVariable("INTEGRATION_TEST", "True");
         }
     }
 }
