91c35f3c6c2a870419f158527adf454ac1c6299c
Q117 <73116785+KoT-Jmot@users.noreply.github.com>
2023-07-02 22:15:15 +0300

    Create FactoryConfiguration.cs

---
+++ MeetUp.IdentityService/MeetUp.IdentityService.Tests/IntegrationTests/FactoryConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace MeetUp.IdentityService.Tests.IntegrationTests
+{
+    public static class FactoryConfiguration
+    {
+        public static WebApplicationFactory<Program> WebApplicationFactoryConfig(this WebApplicationFactory<Program> webHost)
+        {
+            webHost.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureServices(services =>
+                {
+
+                });
+            });
+
+            return webHost;
+        }
+    }
+}
