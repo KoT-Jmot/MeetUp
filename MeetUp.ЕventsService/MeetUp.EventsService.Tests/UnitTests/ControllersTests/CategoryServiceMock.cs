90f4a8b4a75c426e0e0d4b6a532d07dcecc094f5
Q117 <73116785+KoT-Jmot@users.noreply.github.com>
2023-07-03 19:30:55 +0300

    add controllers tests

---
+++ MeetUp.ЕventsService/MeetUp.EventsService.Tests/UnitTests/ControllersTests/CategoryServiceMock.cs
@@ -0,0 +1,41 @@
+﻿using MeetUp.EventsService.Application.Contracts;
+using MeetUp.EventsService.Application.DTOs.InputDto.CategoryDto;
+using Moq;
+
+namespace MeetUp.EventsService.Tests.UnitTests.ControllersTests
+{
+    public static class CategoryServiceMock
+    {
+        public static Mock<ICategoryService> Create()
+        { 
+            var categoryService = new Mock<ICategoryService>();
+
+            categoryService.Setup(r => r.GetAllCategoriesAsync(
+                            It.IsAny<CategoryQueryDto>(),
+                            It.IsAny<CancellationToken>()))
+                           .ReturnsAsync(DataFactory.GetAllOutputCategoriesDto());
+
+            categoryService.Setup(r => r.GetCategoryByIdAsync(
+                            DataFactory.GetCategoryEntity().Id,
+                            It.IsAny<CancellationToken>()))
+                           .ReturnsAsync(DataFactory.GetOutputCategoryDto());
+
+            categoryService.Setup(r => r.CreateCategoryAsync(
+                            DataFactory.GetCategoryDto(),
+                            It.IsAny<CancellationToken>()))
+                           .ReturnsAsync(DataFactory.GetCategoryEntity().Id);
+
+            categoryService.Setup(r => r.DeleteCategoryByIdAsync(
+                            It.IsAny<Guid>(),
+                            It.IsAny<CancellationToken>()));
+
+            categoryService.Setup(r => r.UpdateCategoryByIdAsync(
+                            It.IsAny<Guid>(),
+                            DataFactory.GetCategoryDto(),
+                            It.IsAny<CancellationToken>()))
+                           .ReturnsAsync(Guid.NewGuid());
+
+            return categoryService;
+        }
+    }
+}
