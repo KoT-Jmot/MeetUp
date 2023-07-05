d805133da986e34f96391279efac2bfd5d98209e
Q117 <73116785+KoT-Jmot@users.noreply.github.com>
2023-07-05 18:52:49 +0300

    Update RepositoryManagerMock.cs

--- MeetUp.CommentsService/MeetUp.CommentsService.Tests/UnitTests/ServicesTests/RepositoryManagerMock.cs
+++ MeetUp.CommentsService/MeetUp.CommentsService.Tests/UnitTests/ServicesTests/RepositoryManagerMock.cs
@@ -1,6 +1,32 @@
-﻿namespace MeetUp.CommentsService.Tests.UnitTests.ServicesTests
+﻿using MeetUp.CommentsService.Infrastructure.Contracts;
+using MockQueryable.Moq;
+using Moq;
+
+namespace MeetUp.CommentsService.Tests.UnitTests.ServicesTests
 {
     public class RepositoryManagerMock
     {
+        public static Mock<IRepositoryManager> Create()
+        {
+            var repositoryManager = new Mock<IRepositoryManager>();
+
+            repositoryManager.Setup(r => r.Comments.GetByIdAsync(
+                               DataFactory.GetCommentEntity().Id,
+                               It.IsAny<bool>(),
+                               It.IsAny<CancellationToken>()))
+                              .ReturnsAsync(DataFactory.GetCommentEntity());
+
+            repositoryManager.Setup(r => r.Comments.GetAll(It.IsAny<bool>()))
+                             .Returns(DataFactory.GetCommentEntities().BuildMock());
+
+            repositoryManager.Setup(r => r.Comments.GetCommentByIdAndUserIdAsync(
+                              DataFactory.GetCommentEntity().UserId,
+                              DataFactory.GetCommentEntity().Id,
+                              It.IsAny<bool>(),
+                              It.IsAny<CancellationToken>()))
+                             .ReturnsAsync(DataFactory.GetCommentEntity());
+
+            return repositoryManager;
+        }
     }
 }
