b1266b8626e81fa39e760b6d9bc8cb6fbdedefba
Q117 <73116785+KoT-Jmot@users.noreply.github.com>
2023-07-05 15:01:30 +0300

    Create CommentValidatorTests.cs

---
+++ MeetUp.CommentsService/MeetUp.CommentsService.Tests/UnitTests/ValidationTests/CommentValidatorTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using MeetUp.CommentsService.Application.DTOs.InputDto;
+using MeetUp.CommentsService.Application.Validation;
+
+namespace MeetUp.CommentsService.Tests.UnitTests.ValidationTests
+{
+    public class CommentValidatorTests
+    {
+        private readonly IValidator<CommentDto> _commentValidator;
+
+        public CommentValidatorTests()
+        {
+            _commentValidator = new CommentValidator();
+        }
+
+        [Theory]
+        [InlineData("1789b1c3-34a2-4f4a-7bbf-08db683498b1", "Hello", true)]
+        [InlineData("1789b1c3-34a2-4f4a-7bbf-08db683498b1", "a", true)]
+        [InlineData("1789b1c3-34a2-4f4a-7bbf-08db683498b1", "", false)]
+        [InlineData("1789b1c3-34a2-4f4a-7bbf-08db683498b1", null, false)]
+        [InlineData(null, "Hello", false)]
+        public void CommentDtoValidatorTests(
+            Guid eventId,
+            string text,
+            bool isValid)
+        {
+            var commentDto = new CommentDto
+            {
+                EventId = eventId,
+                Text = text
+            };
+
+            Assert.Equal(isValid, _commentValidator.Validate(commentDto).IsValid);
+        }
+    }
+}
