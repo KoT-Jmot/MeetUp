1f0f45869df70f94c6691166fb7ec7343b6b0c4e
Q117 <73116785+KoT-Jmot@users.noreply.github.com>
2023-07-05 18:52:31 +0300

    add controller tests

---
+++ MeetUp.CommentsService/MeetUp.CommentsService.Tests/UnitTests/ControllersTests/MockConfigure.cs
@@ -0,0 +1,55 @@
+﻿using MeetUp.CommentsService.Api.Controllers;
+using MeetUp.CommentsService.Application.Contracts;
+using MeetUp.CommentsService.Application.DTOs.InputDto;
+using MeetUp.CommentsService.Application.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace MeetUp.CommentsService.Tests.UnitTests.ControllersTests
+{
+    public static class MockConfigure
+    {
+        public static Mock<ICommentService> CreateCommentService()
+        {
+            var commentService = new Mock<ICommentService>();
+
+
+            commentService.Setup(r => r.GetAllCommentsAsync(
+                           It.IsAny<CommentQueryDto>(),
+                           It.IsAny<CancellationToken>()))
+                          .ReturnsAsync(DataFactory.GetAllOutputCommentsDto());
+
+            commentService.Setup(r => r.GetCommentByIdAsync(
+                           DataFactory.GetCommentEntity().Id,
+                           It.IsAny<CancellationToken>()))
+                          .ReturnsAsync(DataFactory.GetOutputCommentDto());
+
+            commentService.Setup(r => r.CreateCommentByUserIdAsync(
+                           DataFactory.GetUserId,
+                           It.IsAny<CommentDto>(),
+                           It.IsAny<CancellationToken>()))
+                          .ReturnsAsync(DataFactory.GetCommentEntity().Id);
+
+            return commentService;
+        }
+
+        public static void CreateCommentsControllerRequestMock(this CommentsController eventsController)
+        {
+            var mockRequest = new Mock<HttpRequest>();
+            var mockHttpContext = new Mock<HttpContext>();
+
+            mockHttpContext.SetupGet(x => x.Request).Returns(mockRequest.Object);
+
+            mockRequest.Setup(r => r.Headers).Returns(new HeaderDictionary
+            {
+                {"claims_UserId", DataFactory.GetUserId }
+            });
+
+            eventsController.ControllerContext = new ControllerContext
+            {
+                HttpContext = mockHttpContext.Object
+            };
+        }
+    }
+}
