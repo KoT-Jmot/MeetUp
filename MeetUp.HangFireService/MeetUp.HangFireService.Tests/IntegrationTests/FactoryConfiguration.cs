0d3a42ff5dc9a087e3e9015d519168cdd8fe4aa2
Q117 <73116785+KoT-Jmot@users.noreply.github.com>
2023-07-11 23:50:43 +0300

    Update FactoryConfiguration.cs

--- MeetUp.HangFireService/MeetUp.HangFireService.Tests/IntegrationTests/FactoryConfiguration.cs
+++ MeetUp.HangFireService/MeetUp.HangFireService.Tests/IntegrationTests/FactoryConfiguration.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using MeetUp.HangFireSerivce.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace MeetUp.HangFireService.Tests.IntegrationTests
 {
@@ -6,11 +10,29 @@ namespace MeetUp.HangFireService.Tests.IntegrationTests
     {
         public static WebApplicationFactory<Program> WebApplicationFactoryConfig()
         {
-            var webHost = new WebApplicationFactory<Program>().WithWebHostBuilder(_ => { });
+            var webHost = new WebApplicationFactory<Program>().WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureTestServices(services =>
+                {
+                    services.ConfigTestDbContext();
+                });
+            });
 
             ConfigEnvironment();
 
             return webHost;
+        }
+        private static IServiceCollection ConfigTestDbContext(this IServiceCollection services)
+        {
+            var dbContextDescriptor = services.SingleOrDefault(d =>
+                        d.ServiceType.Equals(typeof(DbContextOptions<HangFireContext>)));
+
+            if (dbContextDescriptor != null)
+            {
+                services.Remove(dbContextDescriptor);
+            }
+
+            return services;
         }
 
         private static void ConfigEnvironment()
