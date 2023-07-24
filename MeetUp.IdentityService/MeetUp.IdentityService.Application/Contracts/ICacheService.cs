42c55571f84c4c8dabe34a9214be90b2a327810b
Q117 <vanjapo4ta@gmail.com>
2023-07-24 22:23:19 +0300

    create redis

---
+++ MeetUp.IdentityService/MeetUp.IdentityService.Application/Contracts/ICacheService.cs
@@ -0,0 +1,8 @@
+namespace MeetUp.IdentityService.Application.Contracts;
+
+public interface ICacheService
+{
+    T GetData<T>(string key);
+    bool SetData<T>(string key, T value, DateTimeOffset expirationTime);
+    object RemoveData(string key);
+}
---
+++ MeetUp.IdentityService/MeetUp.IdentityService.Application/Services/CacheService.cs
@@ -0,0 +1,45 @@
+using System.Text.Json;
+using MeetUp.IdentityService.Application.Contracts;
+using StackExchange.Redis;
+
+namespace MeetUp.IdentityService.Application.Services;
+
+public class CacheService : ICacheService
+{
+    private readonly IDatabase _cacheDb;
+    
+    public CacheService()
+    {
+        var redis = ConnectionMultiplexer.Connect("localhost:6379");
+        _cacheDb = redis.GetDatabase();
+    }
+    
+    public T GetData<T>(string key)
+    {
+        var value = _cacheDb.StringGet(key);
+
+        if (!string.IsNullOrEmpty(value))
+        {
+            return JsonSerializer.Deserialize<T>(value);
+        }
+
+        return default;
+    }
+
+    public bool SetData<T>(string key, T value, DateTimeOffset expirationTime)
+    {
+        var expirtyTime = expirationTime.DateTime.Subtract(DateTime.Now);
+         
+        return _cacheDb.StringSet(key, JsonSerializer.Serialize(value), expirtyTime);
+    }
+
+    public object RemoveData(string key)
+    {
+        var exest = _cacheDb.KeyExists(key);
+
+        if (exest)
+            return _cacheDb.KeyDelete(key);
+
+        return false;
+    }
+}
